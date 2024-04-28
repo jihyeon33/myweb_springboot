cc697282b458dc75168e7802b2457cbc06b4d910
jihyeon33 <twinhks1@naver.com>
2024-04-28 20:15:17 +0900

    2024.4.28

--- myweb_springboot/myweb_springboot/src/main/java/com/example/myweb_springboot/controller/TestController.java
+++ myweb_springboot/myweb_springboot/src/main/java/com/example/myweb_springboot/controller/TestController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TestController {
@@ -17,4 +21,23 @@ public class TestController {
     public String getValue(){
         return "test입니다";
     }
+
+    @RequestMapping(value = "/jspTest")
+    public ModelAndView test() throws Exception{
+        ModelAndView mav = new ModelAndView("test");
+        mav.addObject("name", "JIHYEON");
+
+        List<String> testList = new ArrayList<String>();
+        testList.add("a");
+        testList.add("b");
+        testList.add("c");
+
+        mav.addObject("list", testList);
+        return mav;
+    }
+
+/*    @RequestMapping(value = "/jspTest", method = RequestMethod.GET)
+    public String jspTest(){
+        return "test";
+    }*/
 }
